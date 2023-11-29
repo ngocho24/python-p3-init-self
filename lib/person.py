d789e2646049f044587ebf943060a56d3a8ed6e7
Elijah Ngocho <elijahngocho24@gmail.com>
2023-11-29 08:42:05 +0300

    completed

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+# Creating an instance of the Person class
+john = Person("John")
+
+# Accessing the name attribute
+print(john.name)  # Output: John
